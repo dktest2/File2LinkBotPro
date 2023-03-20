b6c91dcf06a850340cb176ac372d57d916cf1622
dktest2 <120356313+dktest2@users.noreply.github.com>
2023-03-20 10:31:24 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -7,5 +7,5 @@ async def start_handler(_, m: Message):
     await m.reply_photo("https://telegra.ph/file/6595b162f49af7e85007c.jpg",
     caption=f'**Hᴇʏ! {m.from_user.mention(style="md")} 😚\n\nI Aᴍ Fɪʟᴇ Lɪɴᴋ Gᴇɴᴇʀᴀᴛᴇʀ Bᴏᴛ 🤖\n\nI Cᴀɴ Sᴛʀᴇᴀᴍ Yᴏᴜʀ Vɪᴅᴇᴏꜱ Tᴏ! ⏯️\n\nSᴇɴᴅ Mᴇ Aɴʏ Fɪʟᴇ Tᴏ Gᴇᴛ Lɪɴᴋ 🔗**',
                                  reply_markup=InlineKeyboardMarkup([
-                                     [InlineKeyboardButton("🔰 Join Our Channel 🔰", url="https://t.me/iPopcornMovie")]
+                                     [InlineKeyboardButton("🔰 Join Our Channel 🔰", url="https://t.me/movie_a1")]
                                  ]))
